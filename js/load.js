c5aad32cb4fed0c6450ce1a9dc7db80a84c8a2a1
henipatel8 <hmanavadaria@gmail.com>
2020-12-07 18:30:28 -0500

    Loading Process Modified

--- js/load.js
+++ js/load.js
@@ -1,2 +1,6 @@
 var newPage = new TimelineLite({ onComplete: newPage });
 newPage.to("#loading_bar", 2.2, { width: "600px", ease: Power0.easeOut });
+
+function newPage() {
+    window.location.replace("/bug.html");
+}
