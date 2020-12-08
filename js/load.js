09647ae056d24e421aca1e03b394de26b3cc2feb
henipatel8 <hmanavadaria@gmail.com>
2020-12-07 19:31:40 -0500

    Loading Process Modified

--- js/load.js
+++ js/load.js
@@ -2,5 +2,5 @@ var newPage = new TimelineLite({ onComplete: newPage });
 newPage.to("#loading_bar", 2.2, { width: "600px", ease: Power0.easeOut });
 
 function newPage() {
-    window.location.replace("/bug.html");
+    window.location.replace("/insect_website_final/bug.html");
 }
